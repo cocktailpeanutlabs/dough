b93751ca460d206662f5e968fd5fc89a58d9a6fa
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-18 11:06:15 -0400

    headless

--- start.js
+++ start.js
@@ -19,7 +19,7 @@ module.exports = () => {
         params: {
           path: project_dir,
           venv: virtual_env,
-          message: "streamlit run app.py --runner.fastReruns false --server.port 5500",
+          message: "streamlit run app.py --server.headless true --runner.fastReruns false --server.port 5500",
           on: [{ event: "/http://[0-9.:]+/", done: true }],
         },
       },
